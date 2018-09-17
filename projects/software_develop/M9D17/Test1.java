81a8107d700fb6cb9da2a9de56efc5359091510a
GodSuperK <iamliuche@163.com>
2018-09-17 21:43:00 +0800

    update 实验1

--- projects/software_develop/M9D17/Test1.java
+++ projects/software_develop/M9D17/Test1.java
@@ -4,32 +4,41 @@ import java.util.Scanner;
 
 /**
  * 实验一: 键盘录入两个数据，比较两个数据(字符串，数值)是否相等
- * Input: 1. 字符串 字符串
- *        2. 字符串 数值
- *        3. 数值 数值
- *        4. 输入当中包含 空 或 空串
- *
- * Process: 1. 使用字符串的静态方法 equals
- *          2. 将两个输入转为同类型：
- *              :) 将字符串转为数值， 然后使用 ==
- *              :) 将数值转为字符串， 然后使用 equals
- *          3. ==
- *          4. 重新获取输入
- * Output:
- *         1.先打印输入及类型
- *         2. 再打印比较结果
  */
 
 class Test1 {
-    public static void main(String[] args) throws Exception{
-        Scanner scanner =new Scanner(System.in);//接收键盘输入的数据
-        System.out.println("请输入数据：");
-        while (scanner.hasNext()) {//现在有输入数据
-            System.out.println("您输入的内容为："+scanner.next());
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("First: ");
+        // 捕获一个字符串，空或空串都不会被捕获
+        String arg1 = in.next();
+        System.out.print("Second: ");
+        String arg2 = in.next();
+        Double arg3 = null;
+        Double arg4 = null;
+        try {
+            // 尝试将输入转为double类型
+            arg3 = Double.parseDouble(arg1);
+            arg4 = Double.parseDouble(arg2);
+            // 进行double类型比较
+            if (arg3 == arg4) {
+                System.out.println("Number1: " + arg3 + "\nNumber2: " + arg4 + "\n数据相等");
+            } else {
+                System.out.println("Number1: " + arg3 + "\nNumber2: " + arg4 + "\n数据不相等");
+            }
+        } catch (NumberFormatException nfe) {
+            // 转换失败后，进行字符串比较
+            if (arg1.equals(arg2)) {
+                System.out.println("String1: " + arg1 + "\nString2: " + arg2 + "\n数据相等");
+            } else {
+                System.out.println("String1: " + arg1 + "\nString2: " + arg2 + "\n数据不相等");
+            }
         }
-        scanner.close();//此处一般省略
     }
-    private String getInputString() {
-        return null;
-    }
+
+
 }
+
+
